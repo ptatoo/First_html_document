ed5d075152e756387c23f1d5beebb3199bcf7079
VeneratedApotheosis <5432154321abcabcabcabcarahesdh@gmail.com>
2025-10-29 16:15:33 -0700

    overhauled scraper to use beautiful soup

--- SoC_Scraper.py
+++ SoC_Scraper.py
@@ -1,27 +1,36 @@
 import csv
 import time
+import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from time import sleep
+from bs4 import BeautifulSoup
+
 
 def parseStatus(text: str):
-    texts = text.split("\n")
-    if texts[0] == "Open":
-        texts[2] = texts[1].split(' ')[0]
-        texts[1] = texts[1].split(' ')[2]
-        return texts
+    texts = text.split("\n",1)
+    status = texts[0]
+    if status[0] == "O":
+        return [status] + re.findall(r'\d+', texts[1])[:2]  
+    elif status == "Closed":
+        return [status] + ((raw := re.findall(r'\d+', texts[1]) + [0])[:1] + [int(raw[1]) + int(raw[0])])[::-1]
     else:
-        return [texts[0], 0, 0]
+        return [status, 0, 0]
 
 def parseTime(text: str):
-    texts = text.split("-")
-    if len(texts) == 1:
-        return ["", ""]
-    texts[0] = texts[0][:-2]
-    texts[1] = texts[1][:-2]
-    return texts
+    lines = text.split("\n")
+    if(len(lines) == 1): 
+        return ['','']
+    returned = ['','']
+    for line in lines:
+        if(line[0] == '-'):
+            returned[1] += line[1:]
+        else:
+            returned[0] += line
+
+    return returned
 
 def parseLocation(text: str):
     texts = text.split("\n")
@@ -53,11 +62,11 @@ def searchSubject(subject: str, sub: str, term: str, driver):
     #find search shadow DOM
     shadow_host = driver.find_element(By.XPATH, '//*[@id="block-ucla-campus-mainpagecontent"]/div[2]/div/div/div/div/ucla-sa-soc-app')
     shadow_root = shadow_host.shadow_root
-
+    
     #search for term and click term
     term_drop_down = shadow_root.find_element(By.ID, 'optSelectTerm')
     term_drop_down.click()
-    specific_term = term_drop_down.find_element(By.CSS_SELECTOR, '[data-yeartext="Winter 2026"]')
+    specific_term = term_drop_down.find_element(By.CSS_SELECTOR, f'[data-yeartext="{term}"]')
     specific_term.click()
 
     #find search bar shadow DOM
@@ -84,8 +93,14 @@ def searchSubject(subject: str, sub: str, term: str, driver):
                 clicked = True
 
     #click go button
-    go_button = shadow_root.find_element(By.CSS_SELECTOR, '[id="div_btn_go"]')
-    go_button.click()
+    try:
+        sleep(0.5)
+        go_button = shadow_root.find_element(By.CSS_SELECTOR, '[id="div_btn_go"]')
+        go_button.click()
+    except:
+        print("dude, ts broke")
+        return False
+    return True
 
 #searches for each sectionand writes it to csv file
 def searchSection(driver, lec_writer):
@@ -96,71 +111,78 @@ def searchSection(driver, lec_writer):
     #find number of pages
     try:
         pg = shadow_root.find_element(By.CSS_SELECTOR, '[class="jPag-pages"]')
-        pgs = pg.find_elements(By.CSS_SELECTOR, '[style="width: 32px;"]')
+        pgB = pg.find_elements(By.CSS_SELECTOR, '[style="width: 32px;"]')
+        pgs = len(pgB)
     except:
-        pgs = [shadow_root]
+        pgs = 1 # if UNO pages
 
-    for num in pgs:
-        if (len(pgs) != 1):
-            num.click()
-        sleep(0.25)
-        class_list = shadow_root.find_element(By.CSS_SELECTOR, '[id="resultsTitle"]')
-        classes = class_list.find_elements(By.CSS_SELECTOR, '[class="row-fluid class-title"]')
+    # loop through each page
+    for i in range(pgs):
+        print(f"page {i + 1} of {pgs}")
+        # get shadow stuff
+        shadow_host = driver.find_element(By.XPATH, '//*[@id="block-ucla-campus-mainpagecontent"]/div[2]/div/div/div/div/ucla-sa-soc-app')
+        shadow_root = shadow_host.shadow_root
 
-        #expand all classes
-        expandAll = shadow_root.find_element(By.CSS_SELECTOR, '[id="expandAll"]')
-        expandAll.click()
-        sleep(0.25)
+        if pgs > 1:
+            # getting current page button :star:
+            cpgB = shadow_root.find_element(By.CSS_SELECTOR, '[class="jPag-pages"]').find_elements(By.CSS_SELECTOR, '[style="width: 32px;"]')
+            cpgB[i].click()
+            sleep(0.5) # Wait for page content to load
 
-        #scrapes each class
-        for cls in classes:
-            class_id = cls.get_attribute("id")
-            #trys to find the class twice, if not prints an error
-            try:
-                rows = cls.find_element(By.CSS_SELECTOR, '[id="' + class_id + '-children"]')
-            except:
-                print("error: " + class_id)
+        # 2. Expand all sections on the current page
+        print("Domain Expansion.")
+        try:
+            expand_all_button = shadow_root.find_element(By.CSS_SELECTOR, '[id="expandAll"]')
+            #JS CLICK!!!
+            driver.execute_script("arguments[0].click();", expand_all_button)
+
+            #waiting for stuff to load
+            sleep(1) 
+        except Exception as e:
+            print(f"expand all btn is like nonexistent bruh")
+            continue
+
+        #steal that HTML
+        print("HEIL HTML")
+        html_content = driver.execute_script("return arguments[0].shadowRoot.innerHTML;", shadow_host) 
+        soup = BeautifulSoup(html_content, 'lxml')
+
+        #parsing
+        class_containers = soup.select('.row-fluid.class-title')
+        for cls in class_containers:
+            class_id = cls.get('id')
+            
+            #gets lec / disc container
+            children_div = soup.find('div', id=f"{class_id}-children")
+            if not children_div:
                 continue
-                    
-            rows = rows.find_elements(By.CSS_SELECTOR, '[class="row-fluid data_row primary-row class-info class-not-checked"]')
-            #each lecture is a row
-            for row in rows:
-                sections = []
-                sections.append(row)
-                #open discussion rows and add them to list
-                disExists = False
-                try:
-                    temp = row.find_element(By.CSS_SELECTOR, '[class="row-fluid data_row secondary-row class-info class-not-checked"]')
-                    disExists = True
-                except:
-                    pass
 
-                if disExists:
-                    secondaryRows = row.find_elements(By.CSS_SELECTOR, '[class="row-fluid data_row secondary-row class-info class-not-checked"]')
-                    for secondaryRow in secondaryRows:
-                        sections.append(secondaryRow)
-                        
-                
-                #gather lecture and discussiondata
-                for section in sections:
+            # find all rows
+            all_rows = children_div.select('.row-fluid.data_row')
+            
+            #parser
+            for row in all_rows:
+                try:
                     section_list = {}
                     section_list["classId"] = class_id
-                    section_list["lec_dis"] = section.find_element(By.CSS_SELECTOR, '[class="sectionColumn"]').text
-                    texts = parseStatus(section.find_element(By.CSS_SELECTOR, '[class="statusColumn"]').text)
-                    section_list["status"] = texts[0]
-                    section_list["total_spots"] = texts[1]
-                    section_list["enrolled_spots"] = texts[2]
-                    section_list["waitlist_status"] = section.find_element(By.CSS_SELECTOR, '[class="waitlistColumn"]').text
-                    section_list["days"] = section.find_element(By.CSS_SELECTOR, '[class="dayColumn hide-small beforeCollapseHide"]').get_attribute("innerText").strip()
-                    timing = section.find_element(By.CSS_SELECTOR, '[class="timeColumn"]').text.split("\n")
-                    texts = parseTime(timing[-1])
-                    section_list["start_time"] = texts[0]
-                    section_list["end_time"] = texts[1]
-                    section_list["location"] = parseLocation(section.find_element(By.CSS_SELECTOR, '[class="locationColumn hide-small"]').text)
-                    section_list["units"] = section.find_element(By.CSS_SELECTOR, '[class="unitsColumn"]').text
-                    section_list["instructors"] = parseInstructor(section.find_element(By.CSS_SELECTOR, '[class="instructorColumn hide-small"]').text)
-                    lec_writer.writerow(section_list)
+                    section_list["lec_dis"] = row.select_one('.sectionColumn a').get_text(strip=True, separator=', ')
 
+                    status_raw = row.select_one('.statusColumn').get_text('\n')
+                    texts = parseStatus(status_raw)
+                    section_list["status"], section_list["enrolled_spots"], section_list["total_spots"] = texts
+                    
+                    section_list["waitlist_status"] = row.select_one('.waitlistColumn').get_text(strip=True)
+                    section_list["days"] = row.select_one('.dayColumn').get_text(strip=True, separator=", ")
+                    
+                    # Handle multi-line time column
+                    section_list["start_time"], section_list["end_time"] = parseTime(row.select_one('div[id*="-days_data"] + p').get_text(separator="\n"))
+                    section_list["location"] = row.select_one('.locationColumn').get_text(separator=', ', strip = True)
+                    section_list["units"] = row.select_one('.unitsColumn').get_text(strip=True)
+                    section_list["instructors"] = row.select_one('.instructorColumn').get_text(separator=', ', strip = True)
+                    lec_writer.writerow(section_list)
+                except AttributeError:
+                    # This happens if a row is missing a column, we can safely skip it
+                    continue
 #searches for subject, searches for each section, writes it to csv file
 def scrapeSubject(subject: str, term, headless: bool):
     start_time = time.perf_counter()
@@ -182,8 +204,8 @@ def scrapeSubject(subject: str, term, headless: bool):
     section_writer.writeheader()
 
     #search and scrape and write
-    searchSubject(subject, "(" + subject + ")", term, driver)
-    searchSection(driver, section_writer)
+    if((searchSubject(subject, "(" + subject + ")", term, driver))):
+        searchSection(driver, section_writer)
     section_file.close()
     sleep(1)
     driver.quit()
@@ -191,12 +213,10 @@ def scrapeSubject(subject: str, term, headless: bool):
     elapsed_time = end_time - start_time
     print(f"Code execution time: {elapsed_time:.4f} seconds")
 
-# subjectList = open("SubjectsList.txt", "r")
-# for line in subjectList:
-#     line = line.strip()
-#     lines = line.split("(")
-#     if (len(lines) > 1):
-#         line = line.split("(")[1][:-1]
-#     scrapeSubject(line, "Winter 2026")
-
-scrapeSubject("Math", "Winter 2026", False)
+subjectList = open("SubjectsList.txt", "r")
+for line in subjectList:
+    line = line.strip()
+    lines = line.split("(")
+    if (len(lines) > 1):
+        line = line.split("(")[1][:-1]
+    scrapeSubject(line, "Winter 2026", False)
