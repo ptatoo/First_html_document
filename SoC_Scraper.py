e66146310e349d995ddb5ac91718538771dd88bd
VeneratedApotheosis <5432154321abcabcabcabcarahesdh@gmail.com>
2025-10-31 17:17:32 -0700

    fixed SOC, not cutting edgeg

--- SoC_Scraper.py
+++ SoC_Scraper.py
@@ -4,9 +4,11 @@ import time
 import re
 from time import sleep
 
-#THREADING
-import logging
-import threading
+import queue
+
+#threads
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 
 #MULTIPRROCESSING
 from concurrent.futures import ProcessPoolExecutor
@@ -23,12 +25,16 @@ from selenium.webdriver.support import expected_conditions as EC
 #beautiful soup
 from bs4 import BeautifulSoup
 
-def waitTillJqueryComplete(driver, timeout=5):
+
+BASE_URL = "https://sa.ucla.edu/ro/public/soc"
+
+#call this if u wanna wait for all jqueryies to finish
+def waitTillJqueryComplete(driver, debug, timeout=15):
     try:
         wait = WebDriverWait(driver,timeout=timeout)
         wait.until(lambda d: d.execute_script("return (typeof jQuery !== 'undefined') && (jQuery.active === 0)"))
     except:
-        print("TS pmo'd the waittimng ro someting")
+        print(f"ERRRRr: {debug}")
         return False
 
 def parseStatus(text: str):
@@ -80,66 +86,68 @@ def scrape_subject_wrapper(job_args):
     return scrapeSubject(*job_args)
 
 #searches for a particular subject
-def searchSubject(subject: str, sub: str, term: str, driver):
-    #open schedule of classes
-    try:
-        driver.get("https://sa.ucla.edu/ro/public/soc")
-    except:
-        raise Exception("Failed to open website link")
-    driver.implicitly_wait(0.5)
+""" args
 
-    #find search shadow DOM
-    shadow_host = driver.find_element(By.XPATH, '//*[@id="block-ucla-campus-mainpagecontent"]/div[2]/div/div/div/div/ucla-sa-soc-app')
-    shadow_root = shadow_host.shadow_root
-    
-    #search for term and click term
-    term_drop_down = shadow_root.find_element(By.ID, 'optSelectTerm')
-    term_drop_down.click()
-    specific_term = term_drop_down.find_element(By.CSS_SELECTOR, f'[data-yeartext="{term}"]')
-    specific_term.click()
+subjectID : ID for subject ex. MATH, COM SCI
+term : term we are searching in ex. 25F, 26W
 
-    #find search bar shadow DOM
-    driver.implicitly_wait(0.5)
-    shadow_content = shadow_root.find_element(By.CSS_SELECTOR, '[id="select_filter_subject"]')
-    shadow_root2 = shadow_content.shadow_root
-    search_bar = shadow_root2.find_element(By.CSS_SELECTOR, '[id="IweAutocompleteContainer"]')
+"""
+def get_url(subjectID : str, term : str):
+    formattedSubj = subjectID.ljust(7,'+')
+    url = (
+        BASE_URL + 
+        f"/Results?t={term}"
+        f"&sBy=subject"
+        f"&subj={formattedSubj}"
+        f"&catlg="
+        f"&cls_no="
+    )
+    return url
 
-    #search for element
-    inpt = search_bar.find_element(By.CSS_SELECTOR, '[type="text"]')
-    inpt.send_keys(subject)
-    drop_down = search_bar.find_element(By.CSS_SELECTOR, '[id="dropdownitems"]')
-    drop_down_items = drop_down.find_elements(By.CSS_SELECTOR, '[tabindex="-1"]')
+#getting  row content given BS of the row
+def get_row_content(row : BeautifulSoup):
+    return {
+        "lec_dis" : row.select_one('.sectionColumn a').get_text(strip=True, separator=', '),
+        "status" : (status := parseStatus(row.select_one('.statusColumn').get_text('\n')))[0],
+        "enrolled_spots" : status[1],
+        "total_spots" : status[2],
+        "waitlist_status" : row.select_one('.waitlistColumn').get_text(strip=True),
+        "days" : row.select_one('.dayColumn').get_text(strip=True, separator=", "),
+        "start_time" : (times := parseTime(row.select_one('div[id*="-days_data"] + p').get_text(separator="\n")))[0],
+        "location" : times[1],
+        "units" : row.select_one('.unitsColumn').get_text(strip=True),
+        "instructors" : row.select_one('.instructorColumn').get_text(separator=', ', strip = True)
+    }
 
-    #click drop down item
-    clicked = False
-    for drop_down_item in drop_down_items:
-        if sub in drop_down_item.text:
-            drop_down_item.click()
-            clicked = True
-    if not clicked:
-        for drop_down_item in drop_down_items:
-            if sub[1:-1] in drop_down_item.text:
-                drop_down_item.click()
-                clicked = True
+def scrapeExpandedHTML(driver, lec_writer, shadow_host):
+    #steal that HTML
+    html_content = driver.execute_script("return arguments[0].shadowRoot.innerHTML;", shadow_host) 
+    soup = BeautifulSoup(html_content, 'lxml')
 
-    #click go button
-    try:
-        go_button = shadow_root.find_element(By.CSS_SELECTOR, '[id="div_btn_go"]')
-        go_button.click()
-    except:
-        print("dude, ts broke")
-        return False
-    return True
+    #parsing
+    class_containers = soup.select('.row-fluid.class-title')
+    for cls in class_containers:
+        class_id = cls.get('id')
+        
+        #gets lec / disc container
+        children_div = soup.find('div', id=f"{class_id}-children")
+        if not children_div:
+            continue
 
-class scrapeThread(threading.Thread):
-    def __init__(self, str, term):
-        threading.Thread.__init__(self)
-        self.str = str
-        self.term = term
-    def run(self):
-        scrapeSubject(self.str, self.term, True)
+        # find all rows
+        all_rows = children_div.select('.row-fluid.data_row')
+        
+        #parser
+        for row in all_rows:    
+            try:
+                section_list = {"classId":class_id, **get_row_content(row)}
+                lec_writer.writerow(section_list)
+            except AttributeError:
+                # This happens if a row is missing a column, we can safely skip it
+                continue
 
-#searches for each sectionand writes it to csv file
+
+#searches for each section and writes it to csv file
 def searchSection(driver, lec_writer, debug):
     #find all sections
     shadow_host = driver.find_element(By.XPATH, '//*[@id="block-ucla-campus-mainpagecontent"]/div[2]/div/div/div/div/ucla-sa-soc-app')
@@ -155,17 +163,20 @@ def searchSection(driver, lec_writer, debug):
 
     # loop through each page
     for i in range(pgs):
-        print(f"page {i + 1} of {pgs}")
         # get shadow stuff
         shadow_host = driver.find_element(By.XPATH, '//*[@id="block-ucla-campus-mainpagecontent"]/div[2]/div/div/div/div/ucla-sa-soc-app')
         shadow_root = shadow_host.shadow_root
 
-        if pgs > 1:
-            # getting current page button :star:
-            cpgB = shadow_root.find_element(By.CSS_SELECTOR, '[class="jPag-pages"]').find_elements(By.CSS_SELECTOR, '[style="width: 32px;"]')
-            cpgB[i].click()
-            #wait for next page to load
-            waitTillJqueryComplete(driver, debug)
+        try:
+            if pgs > 1:
+                # getting current page button :star:
+                cpgB = shadow_root.find_element(By.CSS_SELECTOR, '[class="jPag-pages"]').find_elements(By.CSS_SELECTOR, '[style="width: 32px;"]')
+                cpgB[i].click()
+                #wait for next page to load
+                waitTillJqueryComplete(driver, debug)
+        except Exception as e:
+            print(debug)
+            break
 
     
         # 2. Expand all sections on the current page
@@ -174,52 +185,46 @@ def searchSection(driver, lec_writer, debug):
             #JS CLICK!!!
             driver.execute_script("arguments[0].click();", expand_all_button)
         except Exception as e:
-            print(f"expand all btn is like nonexistent bruh")
+            print(debug)
             continue
         
         #wait for expansion
         waitTillJqueryComplete(driver, debug)
 
-        #steal that HTML
-        html_content = driver.execute_script("return arguments[0].shadowRoot.innerHTML;", shadow_host) 
-        soup = BeautifulSoup(html_content, 'lxml')
+        scrapeExpandedHTML(driver,lec_writer,shadow_host)
 
-        #parsing
-        class_containers = soup.select('.row-fluid.class-title')
-        for cls in class_containers:
-            class_id = cls.get('id')
-            
-            #gets lec / disc container
-            children_div = soup.find('div', id=f"{class_id}-children")
-            if not children_div:
-                continue
+    # loop through each page
+    for i in range(pgs):
+        # get shadow stuff
+        shadow_host = driver.find_element(By.XPATH, '//*[@id="block-ucla-campus-mainpagecontent"]/div[2]/div/div/div/div/ucla-sa-soc-app')
+        shadow_root = shadow_host.shadow_root
 
-            # find all rows
-            all_rows = children_div.select('.row-fluid.data_row')
-            
-            #parser
-            for row in all_rows:
-                try:
-                    section_list = {}
-                    section_list["classId"] = class_id
-                    section_list["lec_dis"] = row.select_one('.sectionColumn a').get_text(strip=True, separator=', ')
+        try:
+            if pgs > 1:
+                # getting current page button :star:
+                cpgB = shadow_root.find_element(By.CSS_SELECTOR, '[class="jPag-pages"]').find_elements(By.CSS_SELECTOR, '[style="width: 32px;"]')
+                cpgB[i].click()
+                #wait for next page to load
+                waitTillJqueryComplete(driver, debug)
+        except Exception as e:
+            print(debug)
+            break
 
-                    status_raw = row.select_one('.statusColumn').get_text('\n')
-                    texts = parseStatus(status_raw)
-                    section_list["status"], section_list["enrolled_spots"], section_list["total_spots"] = texts
-                    
-                    section_list["waitlist_status"] = row.select_one('.waitlistColumn').get_text(strip=True)
-                    section_list["days"] = row.select_one('.dayColumn').get_text(strip=True, separator=", ")
-                    
-                    # Handle multi-line time column
-                    section_list["start_time"], section_list["end_time"] = parseTime(row.select_one('div[id*="-days_data"] + p').get_text(separator="\n"))
-                    section_list["location"] = row.select_one('.locationColumn').get_text(separator=', ', strip = True)
-                    section_list["units"] = row.select_one('.unitsColumn').get_text(strip=True)
-                    section_list["instructors"] = row.select_one('.instructorColumn').get_text(separator=', ', strip = True)
-                    lec_writer.writerow(section_list)
-                except AttributeError:
-                    # This happens if a row is missing a column, we can safely skip it
-                    continue
+    
+        # 2. Expand all sections on the current page
+        try:
+            expand_all_button = shadow_root.find_element(By.CSS_SELECTOR, '[id="expandAll"]')
+            #JS CLICK!!!
+            driver.execute_script("arguments[0].click();", expand_all_button)
+        except Exception as e:
+            print(debug)
+            continue
+        
+        #wait for expansion
+        waitTillJqueryComplete(driver, debug)
+        scrapeExpandedHTML(driver,lec_writer,shadow_host)
+
+
 
 #searches for subject, searches for each section, writes it to csv file
 def scrapeSubject(subject: str, term, headless: bool):
@@ -236,82 +241,75 @@ def scrapeSubject(subject: str, term, headless: bool):
         raise Exception("Failed to run webdriver.")
     driver.implicitly_wait(0.5)
 
+
+    
+    start = time.perf_counter()
     #open up csv file
     section_keys = ["classId", "lec_dis", "status", "total_spots", "enrolled_spots", "waitlist_status", "days", "start_time", "end_time", "location", "units", "instructors"]
     section_file = open("./section_data/" + subject + ".csv", "w", newline='')
     section_writer = csv.DictWriter(section_file, section_keys)
     section_writer.writeheader()
 
-    #search and scrape and write
-    if((searchSubject(subject, "(" + subject + ")", term, driver))):
-        searchSection(driver, section_writer, subject)
+    end = time.perf_counter()
+    elapsed_time = end - start
+
+    print(f"CSV open: {elapsed_time:.4f} seconds")
+    #Search 
+    try:
+        driver.get(get_url(subject, term))
+    except:
+        raise Exception("Failed to open website link")
+
+    #scrape
+    searchSection(driver, section_writer, subject)
+    driver.implicitly_wait(0.5)
     section_file.close()
-    sleep(1)
+    sleep(0.5)
     driver.quit()
     # end_time = time.perf_counter()
     # elapsed_time = end_time - start_time
     # print(f"Code execution time: {elapsed_time:.4f} seconds")
 
 def scrape_subject_wrapper(job_args):
-    """
-    This function is picklable. It takes a tuple of arguments
-    and unpacks them when calling the actual worker function.
-    """
     return scrapeSubject(*job_args)
 
 if __name__ == '__main__':
 
     multiprocessing.set_start_method('spawn')
     start_time = time.perf_counter()
-    subject_list = open("SubjectsList.txt", "r")
-    
+    subject_list = open("ScrapeSubjects.txt", "r")
 
-    # threads = []
-
-    # for i, line in enumerate(subjectList):
-    #     if(i < 2):
-    #         line = line.strip()
-    #         lines = line.split("(")
-    #         if (len(lines) > 1):
-    #             line = line.split("(")[1][:-1]   
-    #         t = scrapeThread(line,"Winter 2026")
-    #         t.start()
-    #         threads.append(t)
-
-    # for i in threads:
-    #     i.join()
-
-    #PROCESS MANAGEMENT STUFF
-    #CREATES LIST OF ARGS FOR EXECUTOR
     jobs = []
 
     for i, line in enumerate(subject_list):
-        if(i < 40):
+        if(i<12):
             line = line.strip()
             lines = line.split("(")
             if (len(lines) > 1):
                 line = line.split("(")[len(lines) - 1][:-1]   
-            arg = (line,"Winter 2026",True)
+            arg = (line,"25F",False)
             jobs.append(arg)
 
     print("args: ")
     print(jobs)
     print("-" * 50)
-
+    
     #SETTING UP PROCESS POOL EXECUTOR
-    max_workers = 20
-
+    jobs = []
+    jobs.append(("MATH","25F",False))
+    max_workers = 1
+    scrapeSubject("MATH","25F",False)
     
 
     #THE PROCESS POOL EXECUTOR
-    with ProcessPoolExecutor(max_workers=max_workers) as executor:
-        print(1)
-        iterator = executor.map(scrape_subject_wrapper,jobs)
-        results = list(iterator)
+    # with ProcessPoolExecutor(max_workers=max_workers) as executor:
+    #     print(1)
+    #     iterator = executor.map(scrape_subject_wrapper,jobs)
+    #     results = list(iterator)
 
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-    print(f"Code execution time: {elapsed_time:.4f} seconds")
+    # end_time = time.perf_counter()
+    # elapsed_time = end_time - start_time
+    # print(f"Code execution time: {elapsed_time:.4f} seconds")
 
     #2 in 10
     #15 in 24
