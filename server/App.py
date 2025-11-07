3438cff2341ca143be3c90fa5649acfce40a53c2
ptatoo <i.alexander.song@gmail.com>
2025-11-06 19:26:17 -0800

    erm revert ig?

--- server/App.py
+++ server/App.py
@@ -22,25 +22,9 @@ def get_data():
         try:
             return open(f"server/section_data/{filePath}", "r").read()
         except:
-            try:
-                return open(f"section_data/{filePath}", "r").read()
-            except:
-                return jsonify("Cannot find file. " + f"server/Section_Data/{filePath}")
+            return jsonify("Cannot find file. " + f"server/Section_Data/{filePath}")
     
     return ""
-
-scheduler = BackgroundScheduler()
-
-# 2. Add the job to the scheduler
-# This will run the 'scrape_data' function every 30 minutes.
-# Use 'seconds', 'minutes', 'hours', 'days', or 'weeks'.
-scheduler.add_job(func=run_task, trigger="interval", seconds = 30)
-
-# 3. Start the scheduler
-scheduler.start()
-
-# 4. Shut down the scheduler when the app exits
-atexit.register(lambda: scheduler.shutdown())
 
 
 if __name__ == '__main__':
