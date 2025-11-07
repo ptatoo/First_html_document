5862cde4c66740ef3bfbec90477b21b2575c64de
VeneratedApotheosis <5432154321abcabcabcabcarahesdh@gmail.com>
2025-11-06 19:36:26 -0800

    cleaned up backend and updated whatever

--- server/App.py
+++ server/App.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from SoC_Scraper import SoC_Scraper
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
 
 app = Flask(__name__)
 
@@ -17,11 +19,27 @@ def get_data():
     if(request.method == 'GET'):
         filePath = request.args['filePath']
         try:
-            return open(f"server/Section_Data/{filePath}", "r").read()
+            return open(f"server/section_data/{filePath}", "r").read()
         except:
             return jsonify("OOPSIE")
     
     return None
 
+scheduler = BackgroundScheduler()
+
+# 2. Add the job to the scheduler
+# This will run the 'scrape_data' function every 30 minutes.
+# Use 'seconds', 'minutes', 'hours', 'days', or 'weeks'.
+scheduler.add_job(func=run_task, trigger="interval", seconds = 30)
+
+# 3. Start the scheduler
+scheduler.start()
+
+# 4. Shut down the scheduler when the app exits
+atexit.register(lambda: scheduler.shutdown())
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
+
+
