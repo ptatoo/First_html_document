b2c77f7d3a36728d4f150407c2bb3718013c8e8a
ptatoo <i.alexander.song@gmail.com>
2025-11-25 20:44:00 -0800

    BOOM

--- server/SoC_Scraper.py
+++ server/SoC_Scraper.py
@@ -51,7 +51,7 @@ def SoC_Scraper(subjects_path, section_directory):
         
         #creates job args from the batches
         job_args = [(batch, "26W", True) for batch in batches]
-        job_args = [(["MATH"], "26W", False)]
+        job_args = [(["MATH"], "26W", True)]
 
         print(f"Starting {len(job_args)} workers to scrape {len(subjectID_list)} subjects.")
         print("-" * 50)
